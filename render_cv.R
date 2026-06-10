61c37749607bccc58c91370047111c066f4e52eb
nyamisi <nyamisip@gmail.com>
2026-06-10 19:54:02 +0300

    primary productio, HABs and zoo taxonomy added

--- render_cv.R
+++
@@ -1,4 +0,0 @@
-if (!requireNamespace("quarto", quietly = TRUE)) {
-  stop("Please install the 'quarto' R package or render the 'cv_from_docx.qmd' with the Quarto CLI (https://quarto.org).")
-}
-quarto::quarto_render("cv_from_docx.qmd")
